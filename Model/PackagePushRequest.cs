3e5ddc67d0c7e577e5fae7bef721b86f9db0563d
prab2014 <67781436+prab2014@users.noreply.github.com>
2020-07-30 15:26:13 +0530

    Update PackagePushRequest.cs

--- Model/PackagePushRequest.cs
+++ Model/PackagePushRequest.cs
@@ -6,20 +6,10 @@ using System.Threading.Tasks;
 namespace SalesforcePushUpgrade.Model
 {
     public class PackagePushRequest
-    {
-        /// <summary>
-        /// The non-beta, non-deprecated package version that the package is being upgraded to.
-        /// </summary>
-        public string PackageVersionId { get; set; }
-        /// <summary>
-        /// The date and time (UTC) at which the push request is processed
-        /// </summary>
-        public DateTime ScheduledStartTime { get; set; }
-        /// <summary>
-        /// The status of the push. Valid values are:  Created(default),Scheduled(pending),In Progress,Succeeded,Failed,Aborthed(Canceled)
-        /// </summary>
+    {       
+        public string PackageVersionId { get; set; }        
+        public DateTime ScheduledStartTime { get; set; }        
         public string Status { get; set; }
-
         public string Id { get; set; }
         public DateTime SystemModstamp { get; set; }
     }
